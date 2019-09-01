1467b9804987e4d1d3f2093211fbfc42506d1871
Gabe Gutierrez <gabedotgutierrez@gmail.com>
2019-09-01 22:31:15 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
-# Add your variables here
+first_number = 12
+second_number = 25 
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+puts sum  
+puts difference 
+puts product 
+puts quotient
